4bf300bfea733e9c2967e11d227143c25e624450
masahiro-hoshino-921 <hoshino-masahiro921@g.ecc.u-tokyo.ac.jp>
2026-05-09 23:41:14 +0900

    seminar added

--- js/presentations.js
+++ js/presentations.js
@@ -4,6 +4,20 @@
 const presentationsData = [
     {
         type: "seminar",
+        title: "Universal behaviors of quantum magic in many-body systems",
+        authors: "Masahiro Hoshino, Ryota Matsuda, Masaki Oshikawa, Yuto Ashida",
+        venue: "High energy physics theory group seminar",
+        location: "YITP, Kyoto University, Japan",
+        date: "2026-5-8",
+        format: "oral",
+        status: "completed",
+        abstract: "Along with entanglement, the nonstabilizerness (or magic) of quantum systems has been recognized as a crucial resource for achieving quantum computational advantage. The stabilizer Rényi entropy (SRE) has recently been established as a computationally tractable measure of nonstabilizerness, with numerical studies revealing universal behavior of the SRE in critical quantum spin chains. In this work, we unveil the origin of this universality through boundary conformal field theory calculations of the SRE. Our analysis demonstrates that the SRE of the entire system contains a universal constant term linked to the g-factor of the boundary condition imposed by Bell-state measurements, while the mutual SRE exhibits universal logarithmic scaling with a coefficient determined by the scaling dimension of a boundary operator. These findings establish a field-theoretical framework for understanding the universal features of nonstabilizerness in quantum many-body systems. We will also discuss extension of our framework to resource theories of non-Gaussianity for bosonic and fermionic systems.",
+        slides: "",
+        video: "",
+        poster: ""
+    },
+    {
+        type: "seminar",
         title: "Quantum magic and Conformal Field Theory",
         authors: "Masahiro Hoshino, Masaki Oshikawa, Yuto Ashida",
         venue: "M A Rajabpour group seminar, Fluminense Federal University, Brazil",
